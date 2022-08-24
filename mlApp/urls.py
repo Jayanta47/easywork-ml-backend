3fbd03adc3db20998f342827577a09e5184cfa19
Jayanta Sadhu <jayantasadhu4557@gmail.com>
2022-08-24 16:20:26 +0600

    ml algorithms added and checked

---
+++ mlApp/urls.py
@@ -0,0 +1,31 @@
+from django.urls import path, include
+
+from . import views
+
+# from django.conf.urls import url
+from rest_framework.routers import DefaultRouter
+
+from mlApp.views import EndpointViewSet
+from mlApp.views import MLAlgorithmViewSet
+from mlApp.views import MLAlgorithmStatusViewSet
+from mlApp.views import MLRequestViewSet
+
+router = DefaultRouter(trailing_slash=False)
+router.register(r"endpoints", EndpointViewSet, basename="endpoints")
+router.register(r"mlalgorithms", MLAlgorithmViewSet, basename="mlalgorithms")
+router.register(r"mlalgorithmstatuses", MLAlgorithmStatusViewSet, basename="mlalgorithmstatuses")
+router.register(r"mlrequests", MLRequestViewSet, basename="mlrequests")
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
+# urlpatterns = [
+#     path('endpoints/', views.EndpointViewSet.as_view()),
+#     path('mlalgorithms/', views.MLAlgorithmViewSet.as_view()),
+#     path('mlalgorithmstatuses/', views.MLAlgorithmStatusViewSet.as_view()),
+#     path('mlrequests/', views.MLRequestViewSet.as_view()),
+    
+# ]
+
+
